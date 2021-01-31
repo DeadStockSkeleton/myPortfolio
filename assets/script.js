0e8f9c69038294f3ee4087644b481ff0be25f394
Iyana Medlock <iyanamedlock@gmail.com>
2021-01-31 01:13:06 -0600

    removed fints because 404 error, changed sections into html files instead

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 Splitting();
+
+$('.nav-link').on('click', function(){
+    var active = $('.active');
+
+    if (active.length > 0) {
+        $(active[0]).removeClass('active');
+    }
+
+    $(this).addClass('active');
+
+});
