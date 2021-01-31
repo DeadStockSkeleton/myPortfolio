ab8a17078181ae175b3e955687a61d3b90e3c287
Iyana Medlock <iyanamedlock@gmail.com>
2021-01-31 13:26:00 -0600

    fixed https error, changed navbar ui

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,4 @@
 Splitting();
 
-$('.nav-link').on('click', function(){
-    var active = $('.active');
 
-    if (active.length > 0) {
-        $(active[0]).removeClass('active');
-    }
-
-    $(this).addClass('active');
-
-});
 
