befed8431bf7ac03873d076f960e3f533f31e020
DeadStockSkeleton <51370383+DeadStockSkeleton@users.noreply.github.com>
2021-02-01 13:57:19 -0600

    work section fully finished and operational

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,35 @@ let navLinks = $('.nav-link');
 $(document).ready(function(){
   Splitting();
 
+  let index = 1;
+  showSlide(index);
+
+  function plus(n){
+    showSlide(index += n);
+  }
+$('.prev').on('click', function(){
+  plus(-1);
+})
+$('.next').on('click', function(){
+  plus(1);
+})
+  function showSlide(n){
+    let slides =  $('.slides');
+
+    if (n > slides.length){
+      index = 1
+    }
+
+    if (n < 1){
+      index = slides.length
+    }
+
+    for (let i = 0; i < slides.length; i++){
+      $(slides[i]).css('display', 'none');
+    }
+
+    $(slides[index-1]).css('display', 'block');
+  }
 
     $(navLinks).on('click', function () {
         var active = $('.active');
