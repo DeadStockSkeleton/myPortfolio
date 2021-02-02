6b7840d671c717d60a6dd63806b5e8ad59547c02
Iyana Medlock <iyanamedlock@gmail.com>
2021-02-01 18:58:12 -0600

    formatted all files

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,75 @@
+let navLinks = $(".nav-link");
 
-
-let navLinks = $('.nav-link');
-
-$(document).ready(function(){
+$(document).ready(function () {
   Splitting();
 
   let index = 1;
   showSlide(index);
 
-  function plus(n){
-    showSlide(index += n);
+  function plus(n) {
+    showSlide((index += n));
   }
-$('.prev').on('click', function(){
-  plus(-1);
-})
-$('.next').on('click', function(){
-  plus(1);
-})
-  function showSlide(n){
-    let slides =  $('.slides');
+  $(".prev").on("click", function () {
+    plus(-1);
+  });
+  $(".next").on("click", function () {
+    plus(1);
+  });
+  function showSlide(n) {
+    let slides = $(".slides");
 
-    if (n > slides.length){
-      index = 1
+    if (n > slides.length) {
+      index = 1;
     }
 
-    if (n < 1){
-      index = slides.length
+    if (n < 1) {
+      index = slides.length;
     }
 
-    for (let i = 0; i < slides.length; i++){
-      $(slides[i]).css('display', 'none');
+    for (let i = 0; i < slides.length; i++) {
+      $(slides[i]).css("display", "none");
     }
 
-    $(slides[index-1]).css('display', 'block');
+    $(slides[index - 1]).css("display", "block");
   }
 
-    $(navLinks).on('click', function () {
-        var active = $('.active');
-        if (active.length > 0) {
-            $(active[0]).removeClass('active');
-        }
-        $(this).addClass('active');
-        $('#main-home').css('display', 'none');
-      });
+  $(navLinks).on("click", function () {
+    var active = $(".active");
+    if (active.length > 0) {
+      $(active[0]).removeClass("active");
+    }
+    $(this).addClass("active");
+    $("#main-home").css("display", "none");
+  });
 
-      $('#about-btn').on('click', function() {
-          $('.main-section').css('display', 'none');
-          $('#main').css('display', 'block');
-          $('.about-section-container').css('display', 'block');
-          $('.work-section-container').css('display', 'none');
-          $('.contact-section-container').css('display', 'none');
-      });
-      $('#work-btn').on('click', function() {
-        $('.main-section').css('display', 'none');
-        $('#main').css('display', 'block');
-        $('.about-section-container').css('display', 'none');
-        $('.work-section-container').css('display', 'block');
-        $('.contact-section-container').css('display', 'none');
-    });
-    $('#contact-btn').on('click', function() {
-      $('.main-section').css('display', 'none');
-      $('#main').css('display', 'block');
-      $('.about-section-container').css('display', 'none');
-      $('.work-section-container').css('display', 'none');
-      $('.contact-section-container').css('display', 'block');
-    });
+  $("#about-btn").on("click", function () {
+    $(".main-section").css("display", "none");
+    $("#main").css("display", "block");
+    $(".about-section-container").css("display", "block");
+    $(".work-section-container").css("display", "none");
+    $(".contact-section-container").css("display", "none");
+  });
+  $("#work-btn").on("click", function () {
+    $(".main-section").css("display", "none");
+    $("#main").css("display", "block");
+    $(".about-section-container").css("display", "none");
+    $(".work-section-container").css("display", "block");
+    $(".contact-section-container").css("display", "none");
+  });
+  $("#contact-btn").on("click", function () {
+    $(".main-section").css("display", "none");
+    $("#main").css("display", "block");
+    $(".about-section-container").css("display", "none");
+    $(".work-section-container").css("display", "none");
+    $(".contact-section-container").css("display", "block");
+  });
 
-      $('.back-btn').on('click', function() {
-        closeBtn();
-        
-      });
+  $(".back-btn").on("click", function () {
+    closeBtn();
+  });
 
-      function closeBtn(){
-        $('.main-section').css('display', 'block');
-        $('#main').css('display', 'none'); 
-      }
-
-
+  function closeBtn() {
+    $(".main-section").css("display", "block");
+    $("#main").css("display", "none");
+  }
 });
