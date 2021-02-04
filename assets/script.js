6d4a97842d95ed33492e24805e65f1cfbb109fed
Iyana Medlock <iyanamedlock@gmail.com>
2021-02-04 16:25:00 -0600

    home page created and finished

--- assets/script.js
+++ assets/script.js
@@ -42,34 +42,5 @@ $(document).ready(function () {
     $("#main-home").css("display", "none");
   });
 
-  $("#about-btn").on("click", function () {
-    $(".main-section").css("display", "none");
-    $("#main").css("display", "block");
-    $(".about-section-container").css("display", "block");
-    $(".work-section-container").css("display", "none");
-    $(".contact-section-container").css("display", "none");
-  });
-  $("#work-btn").on("click", function () {
-    $(".main-section").css("display", "none");
-    $("#main").css("display", "block");
-    $(".about-section-container").css("display", "none");
-    $(".work-section-container").css("display", "block");
-    $(".contact-section-container").css("display", "none");
-  });
-  $("#contact-btn").on("click", function () {
-    $(".main-section").css("display", "none");
-    $("#main").css("display", "block");
-    $(".about-section-container").css("display", "none");
-    $(".work-section-container").css("display", "none");
-    $(".contact-section-container").css("display", "block");
-  });
-
-  $(".back-btn").on("click", function () {
-    closeBtn();
-  });
-
-  function closeBtn() {
-    $(".main-section").css("display", "block");
-    $("#main").css("display", "none");
-  }
+  
 });
